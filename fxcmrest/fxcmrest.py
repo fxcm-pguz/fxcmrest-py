18919eb5bf845e9424a0b7d94a609775ae1f520e
Pawel Guz <pguz@fxcm.com>
2019-03-19 14:50:30 +0200

    switching to in-house socketio

--- fxcmrest/fxcmrest.py
+++ fxcmrest/fxcmrest.py
@@ -1,5 +1,5 @@
 from .config import Config
-from socketIO_client import SocketIO
+from .socketio import SocketIO
 import threading
 import requests
 import json
@@ -34,60 +34,54 @@ class FXCMRest():
 		return self.socket
 	
 	def getSocketId(self):
-		return self.socket._engineIO_session.id
+		return self.socket.sid()
 	
-	def onConnect(self, data=''):
-		logging.info("connected: {0}".format(data))
-		self.headers['Authorization'] = "Bearer {0}{1}".format(self.getSocketId(),self.config.token())
+	def onConnected(self):
+		logging.info("connected")
+		self.headers['Authorization'] = "Bearer {0}{1}".format(
+			self.getSocketId(),self.config.token())
 		self.state = "connected"
 		self.socketReadyEvent.set()
 	
-	def onError(self, data=''):
-		logging.info("error: {0}".format(data))
-		self.state = "error: {0}".format(data)
+	def onError(self, code, reason=''):
+		logging.info("error: {0} {1}".format(code, reason))
+		self.state = "error: {0} {1}".format(code, reason)
 		self.socketReadyEvent.set()
 	
-	def onConnectError(self, data=''):
-		logging.info("connect_error: {0}".format(data))
-		self.state = "connect_error: {0}".format(data)
-		self.socketReadyEvent.set()
+	def onDisconnect(self, code, reason=''):
+		logging.info("disconnected: {0} {1}".format(code, reason))
+		self.state = "disconnected: {0} {1}".format(code, reason)
 	
-	def onDisconnect(self, data=''):
-		logging.info("disconnected: {0}".format(data))
-		self.state = "disconnected"
+	def onMessage(self, message=''):
+		logging.info("message: {0}".format(message))
 	
-	def socketThread(self):
+	def connect(self):
+		logging.info("connect()")
 		self.socketReadyEvent.clear()
-		self.socket = SocketIO(self.url(), params={'access_token':self.config.token(),'agent':self.config.agent()})
-		self.socket.on('connect',self.onConnect)
-		self.socket.on('disconnect',self.onDisconnect)
-		self.socket.on('connect_error',self.onConnectError)
-		self.socket.on('error',self.onError)
-		self.socket.wait()
+		self.socket = SocketIO(self.config,
+			on_connected = self.onConnected,
+			on_message = self.onMessage,
+			on_closed = self.onDisconnect,
+			on_error = self.onError)
+		self.socket.connect()
+		self.socketReadyEvent.wait(30)
+		return self.status()
 	
 	def request(self, method, resource, params = {}):
-		req = requests.Request(method, "{0}{1}".format(self.url(),resource), headers=self.headers)
+		req = requests.Request(method, "{0}{1}".format(
+			self.url(),resource), headers=self.headers)
 		if method is 'GET':
 			req.params = params
 		else:
 			req.data = params
 		prep = requests.Session().prepare_request(req)
 		resp = requests.Session().send(prep)
-		logging.info("request({0},{1},{2}) response({3}:{4})".format(method,resource,params,resp.status_code,resp.text))
+		logging.info("request({0},{1},{2}) response({3}:{4})".format(
+			method,resource,params,resp.status_code,resp.text))
 		return resp
 	
-	def connect(self):
-		logging.info("connect()")
-		self.thread = threading.Thread(target=self.socketThread)
-		self.thread.setDaemon(True)
-		self.thread.start()
-		self.socketReadyEvent.wait(30)
-		return self.status()
-	
 	def disconnect(self):
-		if self.thread:
-			self.socket.disconnect()
-			self.thread.join(15)
+		pass # TODO: implement
 	
 	def status(self):
 		return self.state
